e43b03f9e51f53fda22d13f2e279cb5ce2333982
Mateus <mateus.diell@gmail.com>
2021-01-29 16:33:31 -0300

    bibiliotecas
    up de bibliotecas e codigos de exemplo

--- project/project.ino
+++ project/project.ino
@@ -1,8 +1,8 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <FS.h>
 #include "SPIFFS.h"
 #include <WiFi.h>
+#include "ESPAsyncWebServer.h"
 
 #define PINO_DIM    4
 #define PINO_ZC     2
@@ -20,15 +20,10 @@ const char* ssid     = "ESP32";
 const char* password = "12345678";
 
 // Set web server port number to 80
-WiFiServer server(80);
+AsyncWebServer server(80);
 
-// Variable to store the HTTP request
-String header;
 
 short RELE_1 = 16;
-//variaveis que indicam o núcleo
-static uint8_t taskCoreZero = 0;
-static uint8_t taskCoreOne  = 1;
 
 volatile float tempPROG = 0.0;
 volatile float tempATUAL = 0.0;
@@ -49,6 +44,10 @@ DallasTemperature sensors(&oneWire);
 int brilho = 0;
 int brilho_convertido = 0;
 
+// Set LED GPIO
+const int ledPin = 18;
+
+
 int maxv = 0;
 int minv = 100;
  
@@ -64,6 +63,22 @@ portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 volatile bool isPinHighEnabled = false;
 volatile long currentBrightness = minBrightness;
 
+// Replaces placeholder with LED state value
+String processor(const String& var){
+  Serial.println(var);
+  if(var == "STATE"){
+    String ledState;
+    if(digitalRead(ledPin)){
+      ledState = "ON";
+    }
+    else{
+      ledState = "OFF";
+    }
+    Serial.print(ledState);
+    return ledState;
+  }
+  return String();
+}
 
 void ligaRELE(short pin){
   digitalWrite(pin, HIGH);
@@ -201,6 +216,7 @@ void setup() {
   pinMode(RELE_1, OUTPUT);
   digitalWrite(PINO_DIM, LOW);
   digitalWrite(RELE_1, LOW);
+  pinMode(ledPin, OUTPUT);
 
   Serial.print("Setting AP (Access Point)…");
   // Remove the password parameter, if you want the AP (Access Point) to be open
@@ -213,9 +229,7 @@ void setup() {
   
   Serial.print("AP IP address: ");
   Serial.println(WiFi.softAPIP());
-
   
-  server.begin();
   
  
   Serial.println("Controlando dimmer com esp32");
@@ -336,79 +350,34 @@ void coreTaskOne( void * pvParameters ){
     String taskMessage = "Task running on core ";
     taskMessage = taskMessage + xPortGetCoreID();
     Serial.println(taskMessage);
+    // Route for root / web page
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(SPIFFS, "/style.css", "text/css");
+  });
+
+  // Route to set GPIO to HIGH
+  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, HIGH);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+  
+  // Route to set GPIO to LOW
+  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
+    digitalWrite(ledPin, LOW);    
+    request->send(SPIFFS, "/index.html", String(), false, processor);
+  });
+
+
+  
+  server.begin();
      while(true){
       delay(1);
-        WiFiClient client = server.available();   // Listen for incoming clients
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
-        header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
-          if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
-            client.println("HTTP/1.1 200 OK");
-            client.println("Content-type:text/html");
-            client.println("Connection: close");
-            client.println();
-            
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /26/on") >= 0) {
-              Serial.println("GPIO 26 on");
-            } else if (header.indexOf("GET /26/off") >= 0) {
-              Serial.println("GPIO 26 off");
-            } else if (header.indexOf("GET /27/on") >= 0) {
-              Serial.println("GPIO 27 on");
-            } else if (header.indexOf("GET /27/off") >= 0) {
-              Serial.println("GPIO 27 off");
-            }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-            
-            // Display current state, and ON/OFF buttons for GPIO 26  
-            client.println("<p>GPIO 26 - State </p>");
-            // If the output26State is off, it displays the ON button       
-            
-              
-            client.println("</body></html>");
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
-            break;
-          } else { // if you got a newline, then clear currentLine
-            currentLine = "";
-          }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
-        }
-      }
-    }
-    // Clear the header variable
-    header = "";
-    // Close the connection
-    client.stop();
-    Serial.println("Client disconnected.");
-    Serial.println("");
   }
-    } 
+     
 }
